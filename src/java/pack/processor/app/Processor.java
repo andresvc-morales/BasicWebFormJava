b15e4536ae449cef1fc3f1cd823b80dbc6dce126
Andres Morales <andresvcmorales@gmail.com>
2025-09-19 01:58:39 -0600

    Full Project Upload
    Uploaded a completed java project

---
+++ src/java/pack/processor/app/Processor.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pack.processor.app;
+
+/**
+ *
+ * @author andres
+ */
+public class Processor {
+    private String firstName, lastName, email;
+    private Integer age;
+    
+    public Processor(String firstName, String lastName, Integer age, String email ){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+    
+    public String customeMessage(){
+        return "@ProcessorClass: Hello " + this.firstName + " " + this.lastName + " | Age: " + this.age + " "+ "| Email: " + this.email;
+               
+    }
+}
